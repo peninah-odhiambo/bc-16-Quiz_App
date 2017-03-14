d24d6901d9a9901b2f50e6ab2570aa27a58307af
Peninah <pennie.yna@gmail.com>
2017-03-14 20:15:32 +0300

    Add quiz_list function

--- app.py
+++ app.py
@@ -8,18 +8,9 @@ root_path = os.path.dirname(os.path.abspath(__file__))
 quizzes_path = root_path + '/Quizzes'
 
 
-def take_quiz(quiz_name):
-  with open(quizzes_path + '/' + str(quiz_name) + '.json') as json_data:
-    quiz = json.load(json_data)
-    # print('Herereerere')
-    # print(os.path.abspath('C:/Users/andela/Desktop/Imported_Quizzes'))
-    return (quiz)
-
-# print (take_quiz('body'))
-
 #Import Quizzes quiz import <path_to_quiz_JSON>
 def quiz_import(path_to_quiz_to_import):
-    # TO:DO Add error message for json file not found in destination => Quiz not found. Please check source folder.
+    # TO-DO Add error message for json file not found in destination => Quiz not found. Please check source folder.
     # check if file name ends in .json if it doesnt, then it isnt a json file
     # TO-DO: Add error messafe for incorrect path specified
 
@@ -31,22 +22,34 @@ def quiz_import(path_to_quiz_to_import):
     except shutil.Error:
         return "I can't move the file. You've probably given me an incorrect path"
 
+def quiz_list():
+    #TO-DO: Find out a way of returning both files without the .json 
+    quiz_list_array = os.listdir(quizzes_path)
+    for file in quiz_list_array:
+        if file.endswith(".json"):
+            file_name_without_json = file[:-5] # To get rid of .json (slicing method)
+            print(file_name_without_json)
 
-    
+# print (quiz_import('C:/Users/andela/Desktop/Imported_Quizzes/food.json'))
+# print (quiz_list())
 
+def take_quiz(quiz_name):
+  with open(quizzes_path + '/' + str(quiz_name) + '.json') as json_data:
+    quiz = json.load(json_data)
+    return (quiz)
 
-print (quiz_import('C:/Users/andela/Desktop/Imported_Quizzes/food.json'))
+# print (take_quiz('body'))
 
-
-
-
-# TODO:
-# Import Quizzes quiz import <path_to_quiz_JSON>
-# Copy local quiz into quizzes folder and read through it
-# Take quiz
-
-
-
-
-
+    # TO-DO: Identify the quiz to be taken = quiz_name(DONE) USE OS OR SHUTIL
+    # TO-DO: Add error message for incorrect quiz_name = Quiz doesn't exist
+    # TO-DO: Display the questions and choices of quiz_name
+    # TO-DO: Prompt user for an answer within the choices of the questions displayed
+    # TO-DO: Check on a functionality for skipping options
+    # TO-DO: Program should check whether the answer is correct or incorrect 
+    # TO-DO: Tell user whether the answer is "Correct" or "Incorrect"
+    # TO-DO: Proceed to the nect questions till the end of the questions.
+    # TO-DO: Displays the total results in percentage as an integer i.e. full number
+    # TO-DO: If 0-29% == You've not done well, 30-50% Average 51-100% Clap for yourself
+    # TO-DO: Say Thanks and Prompt user to take another quiz and remind them the command for quizlist
+    # TO-DO: Implement timing
 
