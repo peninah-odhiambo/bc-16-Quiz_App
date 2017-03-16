06c3cb1ca4c324e5fb163fcb78f53c72c4142dd1
Peninah <pennie.yna@gmail.com>
2017-03-16 14:15:27 +0300

    Add break between each question

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import json
 import os
 import shutil
+import time
 
 # Read JSON files from Quizzes folder
 root_path = os.path.dirname(os.path.abspath(__file__))
 quizzes_path = root_path + '/Quizzes'
-
 
 #Import Quizzes quiz import <path_to_quiz_JSON>
 def quiz_import(path_to_quiz_to_import):
@@ -42,19 +42,28 @@ def take_quiz(quiz_name):
             questions_triggered = 0
             score = 0
             # percentage_score =  int(round(score/questions_triggered))*100
-            # print ("You scored", score, "out of", questions_triggered, ' ', percentage_score, '%\n' )
+            # print ("You scored", score, "out of", questions_triggered, ' ', percentage_score, '%\n' )    
 
+            print ('\n')
 
+            # start_time = time.time()
+
+            # duration = 5*len(questions)
+
+            # while position < len(questions)
             while questions_triggered < len(quiz_to_take):
+
                 for question in quiz_to_take:
+                    print ('\n')
                     print("Please choose an option")
                     print(question["Question"])
 
-
                     for key,value in question["choices"].items():
                         print(key, value)
+
     
-                    user_input = input("What is your answer?")
+                    user_input = input("What is your answer?") 
+
                     if (user_input.capitalize() == question["correctAnswer"]):
                         print("Good job. You've gotten the correct answer!")
                         questions_triggered += 1
@@ -73,11 +82,29 @@ def take_quiz(quiz_name):
 
                         print ("You scored", score, "out of", questions_triggered) 
                         # percentage = score/questions_triggered * 100
-                        # print (percentage) 
+                        # print (percentage)
+
+
+                print ('\n') 
+
                 percentage = score/questions_triggered * 100
                 percentage = int(percentage)
-                print ("Your total percentage is {} %".format(percentage))     
-                        # break 
+                print ("Total Score percentage is {} %".format(percentage))   
+
+            print ('\n')
+
+             
+
+        # def countdown (count):
+        #     while (count >= 0):
+        #         print ("The count is : ", count)
+        #         count -= 10
+        #         time.sleep (1)
+
+        # countdown (30)
+        # print ("Time Up!")
+
+                    # break 
 
 
     except:
@@ -89,15 +116,15 @@ def take_quiz(quiz_name):
 
     # TO-DO: Identify the quiz to be taken = quiz_name(DONE)
     # TO-DO: Add error message for incorrect quiz_name = Quiz doesn't exist(DONE) 
-    # TO-DO: Display one question and its choices of quiz_name ( DONE BUT FIGURE OUT TO BRING ONE AT A TIME)
+    # TO-DO: Display one question and its choices of quiz_name ( DONE)
     # TO-DO: Prompt user for an answer within the choices of the questions displayed (DONE)
     # TO-DO: Check on a functionality for skipping options
     # TO-DO: Program should check whether the answer is correct or incorrect (DONE)
     # TO-DO: Tell user whether the answer is "Correct" or "Incorrect" (DONE)
     # TO-DO: Proceed to the nect questions till the end of the questions. (DONE)
-    # TO-DO: Displays the total results in percentage as an integer i.e. full number
+    # TO-DO: Displays the total results in percentage as an integer i.e. full number(DONE)
     # TO-DO: If 0-29% == You've not done well, 30-50% Average 51-100% Clap for yourself
-    # TO-DO: Say Thanks and Prompt user to take another quiz and remind them the command for quizlist
+    # TO-DO: Say Thanks and Prompt user to take another quiz and remind them the command for quizlist(DONE)
     # TO-DO: Implement timing
     # TO-DO: Fix looping issue = stop it (DONE)
     # TO-DO: Display right answer when wrong (DONE)
