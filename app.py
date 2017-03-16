a958b854baaa2009c9c9ac21806b6684675244b0
Peninah <pennie.yna@gmail.com>
2017-03-16 20:19:07 +0300

    Add termcolor features

--- app.py
+++ app.py
@@ -2,35 +2,32 @@ import json
 import os
 import shutil
 import time
+from termcolor import cprint, colored
+from pyfiglet import Figlet,figlet_format
 
-# Read JSON files from Quizzes folder
+
 root_path = os.path.dirname(os.path.abspath(__file__))
 quizzes_path = root_path + '/Quizzes'
 
-#Import Quizzes quiz import <path_to_quiz_JSON>
-def quiz_import(path_to_quiz_to_import):
-    # TO-DO Add error message for json file not found in destination => Quiz not found. Please check source folder.
-    # check if file name ends in .json if it doesnt, then it isnt a json file
-    # TO-DO: Add error messafe for incorrect path specified
 
-    # Should copy quiz from path to quiz and paste it in /Quizzes folder
+def quiz_import(path_to_quiz_to_import):
+    """Description: Use command to import quiz from external location.
+       Usage: quiz_import <path_to_quiz_JSON>
+    """
     try:
         absolute_path_to_quiz_to_import = os.path.abspath(path_to_quiz_to_import)
-        shutil.move(path_to_quiz_to_import, quizzes_path)
-        return "I have moved the file"
+        shutil.copy(path_to_quiz_to_import, quizzes_path)
+        return "Quiz successfully copied to local Quizzes folder."
     except shutil.Error:
-        return "I can't move the file. You've probably given me an incorrect path"
+        return "I can't move the file. Please check file import path. \
+                \n Use command quiz_import<path_to_quiz_JSON>"
 
 def quiz_list():
-    #TO-DO: Find out a way of returning both files without the .json 
     quiz_list_array = os.listdir(quizzes_path)
     for file in quiz_list_array:
         if file.endswith(".json"):
             file_name_without_json = file[:-5] # To get rid of .json (slicing method)
             print(file_name_without_json)
-
-# print (quiz_import('C:/Users/andela/Desktop/Imported_Quizzes/food.json'))
-# print (quiz_list())
 
 def take_quiz(quiz_name):
     try:
@@ -41,17 +38,21 @@ def take_quiz(quiz_name):
 
             questions_triggered = 0
             score = 0
-            # percentage_score =  int(round(score/questions_triggered))*100
-            # print ("You scored", score, "out of", questions_triggered, ' ', percentage_score, '%\n' )    
-
+            
             print ('\n')
+            print (colored('-'.center(170,"-"), 'red', attrs=['bold']))
 
             # start_time = time.time()
-
             # duration = 5*len(questions)
+            # while position < len(questions):
+            #     print (question[position])
+            #     print (" ")
+            #     out_of_time = False
 
-            # while position < len(questions)
             while questions_triggered < len(quiz_to_take):
+                # print (questions[position])
+                # print ("")
+                # out_of_time = False
 
                 for question in quiz_to_take:
                     print ('\n')
@@ -59,19 +60,23 @@ def take_quiz(quiz_name):
                     print(question["Question"])
 
                     for key,value in question["choices"].items():
-                        print(key, value)
+                        print(key + " : " + value)
 
     
                     user_input = input("What is your answer?") 
+
+                    # elapsed = time.time() - start_time
+                    # if elapsed > duration:
+                    #     out_of_time = True
+                    #     print ("Time up!")
+                    #     print (EXIT)
 
                     if (user_input.capitalize() == question["correctAnswer"]):
                         print("Good job. You've gotten the correct answer!")
                         questions_triggered += 1
                         score +=1
                         print("Your score is", score)
-                        # percentage = score/questions_triggered * 100
-                        # print (percentage)
-
+                        
     
                     else:
                         print("Sorry. Wrong answer")
@@ -80,17 +85,21 @@ def take_quiz(quiz_name):
                         score = score
                         print("Your score is", score)
 
-                        print ("You scored", score, "out of", questions_triggered) 
-                        # percentage = score/questions_triggered * 100
-                        # print (percentage)
+                    print ("You scored", score, "out of", questions_triggered) 
+                        
 
 
                 print ('\n') 
 
                 percentage = score/questions_triggered * 100
-                percentage = int(percentage)
-                print ("Total Score percentage is {} %".format(percentage))   
+                percentage = int(round(percentage))
+                print("Total Score percentage is {} %".format(percentage))  
 
+                print ('\n')
+
+                print (colored('-'.center(170,"-"), 'green', attrs=['bold']))
+
+                
             print ('\n')
 
              
@@ -111,8 +120,6 @@ def take_quiz(quiz_name):
         return "Error: Quiz of " + quiz_name + " doesn't exist in local quizzes"
         quiz_to_take = json.load(json_data)
         # return quiz_to_take
-
-# take_quiz("football")
 
     # TO-DO: Identify the quiz to be taken = quiz_name(DONE)
     # TO-DO: Add error message for incorrect quiz_name = Quiz doesn't exist(DONE) 
